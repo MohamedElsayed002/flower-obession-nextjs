8fc218d1dd68c74165ddee55acd9e1e6d8316d79
MohamedElsayed002 <mohamed.madoo2016@gmail.com>
2026-04-09 09:03:40 +0200

    remove secret keys :) and unnecessary code

--- app/[locale]/(auth)/login/page.tsx
+++ app/[locale]/(auth)/login/page.tsx
@@ -35,9 +35,6 @@ export default function LoginPage() {
       .regex(/[A-Z]/, { message: t("password-must-contain-at-least-one-uppercase-letter") })
       .regex(/[a-z]/, { message: t("password-must-contain-at-least-one-lowercase-letter") })
       .regex(/[0-9]/, { message: t("password-must-contain-at-least-one-number") })
-      .regex(/[!@#$%^&*()_]/, {
-        message: t("password-must-contain-at-least-one-special-character-and")
-      })
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -76,7 +73,7 @@ export default function LoginPage() {
     } catch { }
   }
 
-  function handleGoogleSignIn(e:React.FormEvent) {
+  function handleGoogleSignIn(e: React.FormEvent) {
     e.preventDefault()
     signInWithGoogle();
   }
@@ -85,97 +82,97 @@ export default function LoginPage() {
 
   return (
     <>
-          <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-96 space-y-4 ">
-        <h1 className="text-4xl text-custom-brown">{t("login")}</h1>
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  autoFocus
-                  tabIndex={1}
-                  aria-label="Email Address"
-                  disabled={isPending}
-                  placeholder={t("enter-your-email-address")}
-                  {...field}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-96 space-y-4 ">
+          <h1 className="text-4xl text-custom-brown">{t("login")}</h1>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    autoFocus
+                    tabIndex={1}
+                    aria-label="Email Address"
+                    disabled={isPending}
+                    placeholder={t("enter-your-email-address")}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    aria-label="Password"
+                    tabIndex={2}
+                    disabled={isPending}
+                    type="password"
+                    placeholder={t("enter-your-password")}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <p className="text-sm text-red-500">{error?.message}</p>
+          <p className="text-sm text-red-500">{googleError?.message}</p>
+          <Button
+            tabIndex={3}
+            aria-label={t("submit")}
+            disabled={isPending}
+            className="w-full bg-custom-brown hover:bg-custom-brown/80"
+            type="submit"
+          >
+            {t("submit")}
+          </Button>
+          <Button
+            type="button"
+            onClick={loginAsTestAdmin}
+            className="w-full border border-custom-brown text-white  hover:bg-custom-brown/10"
+          >
+            {t("login-as-test-admin")}
+          </Button>
+          <Button
+            onClick={handleGoogleSignIn}
+            disabled={isGooglePending}
+            className="w-full border border-custom-brown text-white hover:bg-custom-brown/10"
+          >
+            {isGooglePending ? "Signing in..." : (
+              <span className="flex items-center justify-center gap-2">
+                <img
+                  src="/google-icon.webp"
+                  alt="Google Icon"
+                  className="h-5 w-5"
                 />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  aria-label="Password"
-                  tabIndex={2}
-                  disabled={isPending}
-                  type="password"
-                  placeholder={t("enter-your-password")}
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <p className="text-sm text-red-500">{error?.message}</p>
-        <p className="text-sm text-red-500">{googleError?.message}</p>
-        <Button
-          tabIndex={3}
-          aria-label={t("submit")}
-          disabled={isPending}
-          className="w-full bg-custom-brown hover:bg-custom-brown/80"
-          type="submit"
-        >
-          {t("submit")}
-        </Button>
-        <Button
-          type="button"
-          onClick={loginAsTestAdmin}
-          className="w-full border border-custom-brown text-white  hover:bg-custom-brown/10"
-        >
-          {t("login-as-test-admin")}
-        </Button>
-        <Button
-          onClick={handleGoogleSignIn}
-          disabled={isGooglePending}
-          className="w-full border border-custom-brown text-white hover:bg-custom-brown/10"
-        >
-          {isGooglePending ? "Signing in..." : (
-            <span className="flex items-center justify-center gap-2">
-              <img
-                src="/google-icon.webp"
-                alt="Google Icon"
-                className="h-5 w-5"
-              />
-              {t("sign-in-with-google")}
-            </span>
-          )}
-        </Button>
-        <p className="text-sm text-custom-brown-2">{t("optional-feature")}</p>
-        <div className="-mt-2 text-custom-brown-2">
-          <span> {t("dont-have-an-account")}</span>
-          <Link tabIndex={4} className="underline" href={`/${locale}/register`}>
-            {t("register")}
+                {t("sign-in-with-google")}
+              </span>
+            )}
+          </Button>
+          <p className="text-sm text-custom-brown-2">{t("optional-feature")}</p>
+          <div className="-mt-2 text-custom-brown-2">
+            <span> {t("dont-have-an-account")}</span>
+            <Link tabIndex={4} className="underline" href={`/${locale}/register`}>
+              {t("register")}
+            </Link>
+          </div>
+          <Link
+            tabIndex={5}
+            className="text-custom-brown-2 underline"
+            href={`/${locale}/forgot-password`}
+          >
+            {t("forgot-password")}
           </Link>
-        </div>
-        <Link
-          tabIndex={5}
-          className="text-custom-brown-2 underline"
-          href={`/${locale}/forgot-password`}
-        >
-          {t("forgot-password")}
-        </Link>
-      </form>
-    </Form>
+        </form>
+      </Form>
     </>
   );
 }
--- utils/lib/firebase.ts
+++ utils/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBZgG3CLTl1CGSSiMBJVIemFGDL51FpUJU",
-  authDomain: "booking-app-6a26a.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
   projectId: "booking-app-6a26a",
-  storageBucket: "booking-app-6a26a.firebasestorage.app",
-  messagingSenderId: "639231971358",
-  appId: "1:639231971358:web:c131045504746b166fde0f"
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
