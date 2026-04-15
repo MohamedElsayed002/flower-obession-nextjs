a45df4fd4b757cb7f4f540c66447714308039761
MohamedElsayed002 <mohamed.madoo2016@gmail.com>
2026-04-15 07:08:40 +0200

    caution

--- app/[locale]/(dashbard)/layout.tsx
+++ app/[locale]/(dashbard)/layout.tsx
@@ -6,6 +6,7 @@ import ScrollToTopButton from "@/components/common/scroll-top-button";
 import Footer from "@/components/footer/footer";
 import { Navbar } from "@/components/navbar/navbar";
 import ClientLayout from "@/utils/providers/client-provider";
+import Link from "next/link";
 
 
 
@@ -29,6 +30,12 @@ export default function RootLayout({
     <ClientLayout>
       {/* ✅ Fetch user once here */}
       <div className="flex min-h-screen flex-col">
+        <span className="text-center p-3 bg-red-300">
+          Database was lost due to an outage in AWS Bahrain (me-south-1). Region has been changed, and products will be re-added again 😵‍💫.
+          <Link className="underline font-bold" href="https://github.com/MohamedElsayed002/flower-obession-nextjs" target="_blank" rel="noopener noreferrer">
+            Source Code
+          </Link>
+        </span>
         <header>
           <Navbar />
         </header>
--- utils/lib/firebase.ts
+++ utils/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
+  apiKey: "AIzaSyBZgG3CLTl1CGSSiMBJVIemFGDL51FpUJU",
+  authDomain: "booking-app-6a26a.firebaseapp.com",
   projectId: "booking-app-6a26a",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  storageBucket: "booking-app-6a26a.firebasestorage.app",
+  messagingSenderId: "639231971358",
+  appId: "1:639231971358:web:c131045504746b166fde0f"
 };
 
 // Initialize Firebase
